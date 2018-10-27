229d596ab221558af68764000084d568047b2b22
tmnmn <tmnmn@users.noreply.github.com>
2018-10-28 00:38:55 +0900

    feat: Add sub download feature

--- src/subs-wwdc.rb
+++ src/subs-wwdc.rb
@@ -4,6 +4,8 @@ require 'open-uri'
 require 'nokogiri'
 require 'json'
 require 'fileutils'
+require 'parallel'
+require 'ruby-progressbar'
 
 class SessionInfo
   attr_accessor :title, :genre, :url, :m3uUrl, :summaryText, :sessionDetail
@@ -120,6 +122,7 @@ end
 
 class SubDownloader
   def self.download(m3uUrl, dirPath)
+    retry_count = 0
     begin
       filename = File.join(dirPath, File.basename(m3uUrl))
       open(m3uUrl) do |file|
@@ -129,8 +132,10 @@ class SubDownloader
       end
     rescue => e
       p "! #{m3uUrl} #{filename} (#{e})"
+      sleep(1)
+      retry_count = retry_count + 1
+      retry if retry_count < 3
     end
-    return filename
   end
 end
 
@@ -260,6 +265,116 @@ def download_sub_playlists(filePath)
   end
 end
 
+def enumerate_sub_urls(subPlaylistPath, urlIndexFilePath)
+  subUrls = []
+
+  fullUrl = ""
+  open(urlIndexFilePath, "r") do |f|
+    f.each_line do |line|
+      fullUrl = line
+    end
+  end
+
+  # e.g. sub play list
+  #
+  # #EXTM3U
+  # #EXT-X-TARGETDURATION:60
+  # #EXT-X-VERSION:3
+  # #EXT-X-MEDIA-SEQUENCE:0
+  # #EXT-X-PLAYLIST-TYPE:VOD
+  # #EXTINF:60.00000,
+  # fileSequence0.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence1.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence2.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence3.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence4.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence5.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence6.webvtt
+  # #EXTINF:60.00000,
+  # fileSequence7.webvtt
+  # #EXTINF:43.65700,
+  # fileSequence8.webvtt
+  # #EXT-X-ENDLIST
+
+  open(subPlaylistPath, "r") do |f|
+    f.each_line do |line|
+      subFile = line.chomp
+      if subFile.end_with?('.webvtt')
+        baseUrl = File.dirname(fullUrl)
+        subUrl = File.join(baseUrl, subFile)
+        subUrls.push(subUrl)
+      end
+    end
+  end
+
+  return subUrls
+end
+
+def enumerate_sub_playlists(rootDirPath)
+  subPlaylists = []
+
+  sessionDirPaths = Dir.entries(rootDirPath) - [".", ".."]
+
+  sessionDirPaths.each do |sessionDirPath|
+    sessionDirFullPath = File.expand_path(sessionDirPath, rootDirPath)
+    if File.directory?(sessionDirFullPath)
+      subsDirPaths = Dir.entries(sessionDirFullPath) - [".", ".."]
+
+      subsDirPaths.each do |subsDir|
+        subsDirFullPath = File.expand_path(subsDir, sessionDirFullPath)
+        if File.directory?(subsDirFullPath)
+          langDirs = Dir.entries(subsDirFullPath) - [".", ".."]
+
+          langDirs.each do |langDir|
+            langDirFullPath = File.expand_path(langDir, subsDirFullPath)
+            if File.directory?(langDirFullPath)
+              filePaths = Dir.entries(langDirFullPath) - [".", ".."]
+
+              filePaths.each do |filePath|
+                fileFullPath = File.expand_path(filePath, langDirFullPath)
+                if File.extname(fileFullPath) == ".m3u8"
+                  subPlaylists.push(fileFullPath)
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  return subPlaylists
+end
+
+def download_subs(rootDirPath)
+  downloadSubUrls = []
+
+  subPlaylistPaths = []
+  subPlaylistPaths = enumerate_sub_playlists(rootDirPath)
+  subPlaylistPaths.each do |subPlaylistPath|
+    subPlaylistDirPath = File.dirname(subPlaylistPath)
+    urlIndexFilePath = File.join(subPlaylistDirPath, "url")
+
+    # number of `subUrls` is approx 10000 files
+    subUrls = []
+    subUrls = enumerate_sub_urls(subPlaylistPath, urlIndexFilePath)
+    subUrls.each do |subUrl|
+      downloadSubUrls.push({"url" => subUrl , "dir" => subPlaylistDirPath})
+    end
+  end
+
+  results = Parallel.map(downloadSubUrls, in_threads: 8, progress: "download") { |downloadSubUrl|
+    # download subs
+    SubDownloader.download(downloadSubUrl["url"], downloadSubUrl["dir"])
+  }
+end
+
 # main
 def main (argv)
   wwdcyear = "wwdc2018"
@@ -274,8 +389,11 @@ def main (argv)
   when 'downloadSubPlaylists'
     download_sub_playlists(filePath)
     exit true
+  when 'downloadSubs'
+    download_subs(File.dirname(filePath))
+    exit true
   else
-    puts "sub commands: `createIndexFile` `downloadMasterPlaylists` `downloadSubPlaylists` `generateSubs`"
+    puts "sub commands: `createIndexFile` `downloadMasterPlaylists` `downloadSubPlaylists` `downloadSubs` `generateSubs`"
     exit false
   end
 end
